523c75fe89a9bc858ce48250472515b03b8123b2
DuongThiXuanQuynh <43758300+DuongThiXuanQuynh@users.noreply.github.com>
2019-07-11 16:23:37 +0700

    Xóa file admin nháp ban đầu

--- admin/menu.php
+++
@@ -1,5 +0,0 @@
-<a href="./">Trang chủ </a>
-<a href="./ListTheLoai.php">Quản lý thể loại</a>
-<a href="./ListLoaiTin.php">Quản lý loại tin </a>
-<a href="./ListTinTuc.php">Quản lý tin tức </a>
-<a href="./ListQuangCao.php">Quản lý quảng cáo </a>
